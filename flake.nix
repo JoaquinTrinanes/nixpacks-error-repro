d11aa5bcfaf3de2ba32d6ad4f146c7c23bc62501
Joaquín Triñanes <hi@joaquint.io>
2024-11-21 22:22:14 +0100

    Add nix devShell

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "Description for the project";
+
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs =
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = inputs.nixpkgs.lib.systems.flakeExposed;
+      perSystem =
+        {
+          pkgs,
+          ...
+        }:
+        {
+          devShells.default = pkgs.mkShell {
+            packages = with pkgs; [ nixpacks ];
+          };
+        };
+    };
+}
